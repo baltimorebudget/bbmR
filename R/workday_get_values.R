80d147386cc2d199adf534b26d76772660973b19
Sara Brumfield <sarabrumfield@gmail.com>
2023-06-13 11:52:50 -0400

    changed appropr file

--- R/workday_get_values.R
+++ R/workday_get_values.R
@@ -16,14 +16,11 @@ workday_get_values <- function() {
          `Workday Fund` = as.numeric(`Workday Fund`))
 
   x_spend_cat <- import("G:/Fiscal Years/SubObject_SpendCategory.xlsx")
-  # x_spend_cat <- import("G:/Analyst Folders/Sara Brumfield/_ref/Baltimore FDM Crosswalk.xlsx", which = "Natural") %>%
-  #   select(`Natural`, `Spend Cat`, `SC ID`)
 
   x_cc <- import("G:/Analyst Folders/Sara Brumfield/_ref/Baltimore FDM Crosswalk.xlsx", which = "Pgm-Activity") %>%
     select(`Pgm-Activity`, `CCA ID`, `CC Name`)
 
   x_grant <- import("G:/Analyst Folders/Sara Brumfield/_ref/Baltimore FDM Crosswalk.xlsx", which = "ProjectGrant") %>%
-    # filter(startsWith(`Financial Grant ID`, "GR")) %>%
     select(`Grant / Project`, `Financial Grant ID`, `Financial Grant Name`, `Financials Proj ID`, `Financials Proj Name`, `SpecialPurpose`, `Special Purpose Name`) %>%
     mutate(`Grant / Project` = as.character(`Grant / Project`),
            Grant = as.numeric(substr(`Grant / Project`, 1, 4)))
@@ -32,7 +29,6 @@ workday_get_values <- function() {
     select(`Natural`, `Ledger Acct ID`, `Account Name`, `SC ID`, `Spend Cat`)
 
   x_sp <- readxl::read_excel("Baltimore FDM Crosswalk.xlsx", sheet = "ProjectGrant")%>%
-    # filter(startsWith(`Payroll Grant ID`, "SP")) %>%
     select(`Grant / Project`, `SpecialPurpose`, `Special Purpose Name`)%>%
     mutate(`Grant / Project` = as.character(`Grant / Project`))
 
